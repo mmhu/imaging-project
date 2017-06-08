830fe2803933e4c3188aa88a6942d867b1b4d3da
Catherine's Computer <catscomputer@Catheriness-MacBook-Pro.local>
2017-06-08 15:47:47 -0400

    glcm matrices complete

--- mmhu.R
+++ mmhu.R
@@ -23,3 +23,13 @@ remove.bg <- function(input.img, input.seg) {
 }
 
 lesion.list <- lapply(1:40, function(i) remove.bg(gray.list[[i]], png.list[[i]]))
+
+glcm_list <- lapply(X=lesion.list, FUN=glcm, angle=0, d=1, n_grey=4, normalize=FALSE)
+glrlm_list <- lapply(X=lesion.list, FUN=glrlm, angle=0, n_grey=4)
+glszm_list <- lappy(X=lesion.list, FUN=glszm,n_grey=5)
+mglszm_list <- lapply(X=lesion.list, FUN=mglszm)
+
+glcm_feat_list <- lapply(glcm_list, calc_features)
+glrlm_feat_list <- lapply(glrlm_list, calc_features)
+glszm_feat_list <- lapply(glszm_list, calc_features)
+mglszm_feat_list <- lapply(mglszm_list, calc_features)
